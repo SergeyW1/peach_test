6d2cbe6cc9df922e72697eaee6b72b24bed85569
Sergey Volkov <f.focus1377@gmail.com>
2023-02-12 18:57:56 +0300

    main

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const main = document.querySelector('.main')
+const slidebar = document.querySelector('.slidebar')
+const slidebarActive = document.querySelector('rectangle-active')
 
 main.addEventListener('click', (e) => {
 	if (e.target.tagName === 'A') {
 		e.preventDefault()
 	}
+})
+
+slidebar.addEventListener('click', () => {
+	document.querySelector('.rectangle-no-active').classList.toggle('rectangle-active')
 })
