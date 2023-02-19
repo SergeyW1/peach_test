60082b15d2e7abf5184457e0efd177ec521044d7
Sergey Volkov <f.focus1377@gmail.com>
2023-02-19 21:00:07 +0300

    main

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
-const main = document.querySelector(".main");
+const map = document.querySelector(".map");
 const slidebar = document.querySelector(".slidebar");
 const directBanner = document.querySelector(".directions__banner");
 const bannerSize = document.querySelectorAll(".banner-size");
 const bannerText = document.querySelectorAll(".banner-text");
-const bannerTextDescription = document.querySelectorAll(
-	".banner-text__description"
-);
-const sliderPhoto = document.querySelectorAll('.photo-size')
-const gallerySlider = document.querySelector('.gallery__slider')
+const bannerTextDescription = document.querySelectorAll(".banner-text__description");
+const sliderPhoto = document.querySelectorAll(".photo-size");
+const gallerySlider = document.querySelector(".gallery__slider");
+const rectangleNoActive = document.querySelector(".rectangle-no-active")
+const modal = document.querySelector('.modal')
+const navItem = document.querySelectorAll('.nav-container__item')
+const regions = document.querySelectorAll('.regions__container')
 let dataSizeIndex;
+let dataRegionIndex;
 dataSet();
 
-main.addEventListener("click", (e) => {
-	if (e.target.tagName === "A") {
-		e.preventDefault();
+map.addEventListener("click", (e) => {
+	let region = e.target.dataset.region
+	console.log(region);
+	if (e.target.classList.contains('nav-container__item')) {
+		const el = document.querySelector('.nav-active')
+		if (el) {
+			el.classList.remove('nav-active')
+		}
+		e.target.classList.add('nav-active')
+		regions.forEach(item => {
+			item.classList.add('none')
+		})
+		regions[region - 1].classList.remove('none')
+		if (e.target.dataset.region === '0') {
+			regions.forEach(item => {
+				item.classList.remove('none')
+			})
+		}
 	}
 });
 
 slidebar.addEventListener("click", () => {
-	document
-		.querySelector(".rectangle-no-active")
-		.classList.toggle("rectangle-active");
+	rectangleNoActive.classList.toggle("rectangle-active");
+	modal.classList.toggle('modal-active')
+	if (rectangleNoActive.classList.contains('rectangle-active')) {
+		document.querySelectorAll('.nav-container__item').forEach(item => {
+			if (item.classList.contains('nav-active')) {
+				item.classList.remove('nav-active')
+			}
+		})
+	}
 });
 
 directBanner.addEventListener("mouseover", (e) => {
@@ -28,7 +52,9 @@ directBanner.addEventListener("mouseover", (e) => {
 	if (!data) return;
 	dataSizeIndex = data.dataset.sizeIn;
 	bannerText[dataSizeIndex].classList.add("banner-text__open");
-	bannerTextDescription[dataSizeIndex].classList.add('banner-text__description-active')
+	bannerTextDescription[dataSizeIndex].classList.add(
+		"banner-text__description-active"
+	);
 });
 
 directBanner.addEventListener("mouseout", (e) => {
@@ -36,7 +62,9 @@ directBanner.addEventListener("mouseout", (e) => {
 	if (!data) return;
 	dataSizeIndex = data.dataset.sizeIn;
 	bannerText[dataSizeIndex].classList.remove("banner-text__open");
-	bannerTextDescription[dataSizeIndex].classList.remove('banner-text__description-active')
+	bannerTextDescription[dataSizeIndex].classList.remove(
+		"banner-text__description-active"
+	);
 });
 
 function dataSet() {
@@ -46,13 +74,20 @@ function dataSet() {
 	sliderPhoto.forEach((item, index) => {
 		item.dataset.numPhoto = index + 1;
 	});
+	navItem.forEach((item, index) => {
+		item.dataset.region = index;
+	});
+	regions.forEach((item, index) => {
+		item.dataset.region = index + 1
+	})
 }
 
-$('.gallery-content__photo').slick({
+//Slider Photo
+
+$(".gallery-content__photo").slick({
 	dots: true,
 	infinite: true,
 	speed: 500,
 	fade: true,
-	cssEase: 'linear'
+	cssEase: "linear",
 });
-
